7c39cc15b4caa1e00582e812b9f0dc417e4852fc
Rafael Martins <rrmartins2001@gmail.com>
2024-02-28 20:42:48 -0300

    Dividi as funções em duas - só uma atualização

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-function rankPerWin(wins, losses) {
-  let winBalance = wins - losses
-  let rank
-
-  if (winBalance <= 10) {
-    rank = "Ferro"
-  } else if (winBalance >= 11 && winBalance <= 20) {
-    rank = "Bronze"
-  } else if (winBalance >= 21 && winBalance <= 50) {
-    rank = "Prata"
-  } else if (winBalance >= 51 && winBalance <= 80) {
-    rank = "Ouro"
-  } else if (winBalance >= 81 && winBalance <= 90) {
-    rank = "Diamante"
-  } else if (winBalance >= 91 && winBalance <= 100) {
-    rank = "Landário"
-  } else if (winBalance >= 101) {
-    rank = "Imortal"
-  }
-
-  return `O Herói tem saldo de ${winBalance} e está no nível ${rank}`
-}
-
 // Cuidado => heróis com alta dose de memes de aulas ao vivo da pandemia
 let herosInfo = [
   {
@@ -40,6 +17,35 @@ let herosInfo = [
   }
 ]
 
-for (let player of herosInfo) {
-  console.log(rankPerWin(player.wins, player.losses))
+function calcBalance(wins, losses) {
+  let winBalance = wins - losses
+  return winBalance
 }
+
+function rankPerWin(winBalance) {
+  let rank
+
+  if (winBalance <= 10) {
+    rank = "Ferro"
+  } else if (winBalance >= 11 && winBalance <= 20) {
+    rank = "Bronze"
+  } else if (winBalance >= 21 && winBalance <= 50) {
+    rank = "Prata"
+  } else if (winBalance >= 51 && winBalance <= 80) {
+    rank = "Ouro"
+  } else if (winBalance >= 81 && winBalance <= 90) {
+    rank = "Diamante"
+  } else if (winBalance >= 91 && winBalance <= 100) {
+    rank = "Lendário"
+  } else if (winBalance >= 101) {
+    rank = "Imortal"
+  }
+
+  return rank
+}
+
+herosInfo.forEach(player => {
+  let winBalance = calcBalance(player.wins, player.losses)
+  let rank = rankPerWin(winBalance)
+  console.log(`O Herói ${player.nick} tem saldo de ${winBalance} e está no nível ${rank}`)
+});
